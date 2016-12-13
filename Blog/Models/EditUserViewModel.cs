21c17c1fea1ef4692520c6e9ca608b4f0107e2ef
Kosio996 <bumbletrick@gmail.com>
2016-12-13 19:05:03 +0200

    r

---
+++ Blog/Models/EditUserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Blog.Models;
+
+namespace Blog.Models
+{
+    public class EditUserViewModel
+    {
+        public ApplicationUser User { get; set; }
+
+        public string Password { get; set; }
+
+        [DisplayName("ConfirmPassword")]
+        [Compare("Password", ErrorMessage = "Password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public IList<Role> Roles { get; set; }
+
+       
+
+        
+    }
+}
