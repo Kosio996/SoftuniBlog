eab6e7656d547945fd5379ffea93b3d424846db1
Konstantin <bumbletrick@gmail.com>
2016-12-22 16:29:31 +0200

    No user photo fixed

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -59,7 +59,7 @@ namespace Blog.Controllers
 
                 if (userId == null)
                 {
-                    string fileName = HttpContext.Server.MapPath(@"~/Images/noImg.png");
+                    string fileName = HttpContext.Server.MapPath(@"H:\SoftUniBlog\SoftuniBlog\Blog\Content\images\photo_not_available.png");
 
                     byte[] imageData = null;
                     FileInfo fileInfo = new FileInfo(fileName);
@@ -74,7 +74,18 @@ namespace Blog.Controllers
                 // to get the user details to load user Image
                 var bdUsers = HttpContext.GetOwinContext().Get<BlogDbContext>();
                 var userImage = bdUsers.Users.Where(x => x.Id == userId).FirstOrDefault();
+                if (userImage.UserPhoto == null)
+                {
+                    string fileName = HttpContext.Server.MapPath(@"~/Content/images/photo_not_available.png");
 
+                    byte[] imageData = null;
+                    FileInfo fileInfo = new FileInfo(fileName);
+                    long imageFileLength = fileInfo.Length;
+                    FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                    BinaryReader br = new BinaryReader(fs);
+                    imageData = br.ReadBytes((int)imageFileLength);
+                    return File(imageData, "image/png");
+                }
                 return new FileContentResult(userImage.UserPhoto, "image/jpeg");
             }
             else
